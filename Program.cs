4a7d780eaccffbdf20fb72a67ef1647eb726def6
ialikh72 <ialikh.w1@gmail.com>
2026-01-19 23:38:37 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Sumof2Numnbers(int num1, int num2) // function defination
+    {
+        int mul = num1 * num2;
+        Console.WriteLine("Multiplication of two numbers: " + mul);
+
+    }
+    static void Main()
+    {
+        Sumof2Numnbers(12, 17); // function call by actual parameters
+    }
+}
